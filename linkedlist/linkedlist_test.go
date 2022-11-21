411c65a37c37661040af5be09f911896c5ba9075
souvik.haldar <souvik.haldar@imperva.com>
2022-11-21 18:11:16 +0530

    MAIN Fix stacks and remove questions

--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -10,7 +10,6 @@ func TestInsert(t *testing.T) {
 	l.InsertAtEnd(5)
 	l.InsertAtBeg(0)
 	l.InsertAtPos(4, 4)
-	l.PrintList()
 
 	data, err := l.GetFromPos(1)
 	if err != nil || data != 1 {
@@ -18,15 +17,10 @@ func TestInsert(t *testing.T) {
 	}
 
 	l.DeleteByPos(0)
-	t.Log("After deleting 0th node")
-	l.PrintList()
 	l.DeleteByValue(3)
-	t.Log("After deleting 4th node")
-	l.PrintList()
 
 	t.Log("Reversing")
 	l.Reverse()
-	l.PrintList()
 
 }
 
